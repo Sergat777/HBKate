2809e5004bd1a7d65483d158d14627c102e521bb
Sergat 777 <sergey.kond777@gmail.com>
2022-02-04 14:24:23 +0300

    Add project files.

---
+++ HBKate/Pages/AuthPage.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HBKate.Pages
+{
+    /// <summary>
+    /// Interaction logic for AuthPage.xaml
+    /// </summary>
+    public partial class AuthPage : Page
+    {
+        public AuthPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btYes_Click(object sender, RoutedEventArgs e)
+        {
+            if (MessageBox.Show("Вы Катушка?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                MessageBox.Show("Добро пожаловать!)", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Information);
+                Classes.FrameObj.MainFrame.Navigate(new Pages.CentralPage());
+            }
+            else
+                MessageBox.Show("Вы не Катушка!\nВ доступе отказано!", "Внимание", MessageBoxButton.OK, MessageBoxImage.Stop);
+        }
+
+        private void btNo_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown();
+        }
+    }
+}
---
+++ HBKate/Pages/CentralPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HBKate.Pages
+{
+    /// <summary>
+    /// Interaction logic for CentralPage.xaml
+    /// </summary>
+    public partial class CentralPage : Page
+    {
+        public CentralPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Image_MouseEnter(object sender, MouseEventArgs e)
+        {
+        }
+
+        private void Image_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            if (MessageBox.Show("Вам уже есть 18?", "Внимание", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+            {
+                Classes.SecretWindow secret = new Classes.SecretWindow();
+                secret.Show();
+            }
+            else
+                MessageBox.Show("Тогда что ты тут забыл, малой?", "Уведомление", MessageBoxButton.OK, MessageBoxImage.Question);
+        }
+
+        private void Image_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Classes.GeniusWindow genius = new Classes.GeniusWindow();
+            genius.Show();
+
+        }
+    }
+}
